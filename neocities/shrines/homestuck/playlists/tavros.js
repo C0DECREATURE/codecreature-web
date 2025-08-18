f0e84fa6d1fefc9ddde7fb41123bb1fd45e3bd8e
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2025-08-18 06:11:22 +0100

    tavros playlist added

---
+++ neocities/shrines/homestuck/playlists/tavros.js
@@ -0,0 +1,60 @@
+/*
+<!-- this page's script -->
+<script src="/shrines/homestuck/playlists/sollux.js?fileversion=9"></script>
+
+*/
+
+// force strict mode
+"use strict";
+
+const tavrosPlaylist = {
+	character: 'tavros',
+	title: 'look on the brighter side',
+	playlist: [
+		{
+			title: `Bullfighter Jacket`,
+			artist: `Miniature Tigers`,
+			url: 'https://www.youtube.com/watch?v=V2HdpmK8r2c'
+		},
+		{
+			title: `Hot Venom`,
+			artist: `Miniature Tigers`,
+			url: 'https://www.youtube.com/watch?v=N1l3qZUD28o'
+		},
+		{
+			title: `Yes Man`,
+			artist: `Emma Harner`,
+			url: 'https://www.youtube.com/watch?v=n24rVC4SEg0'
+		},
+		{
+			title: `Tough Guy`,
+			artist: `Cyberbully Mom Club`,
+			url: 'https://www.youtube.com/watch?v=ipW4kUMLXAc'
+		},
+		{
+			title: `Lavender Blood`,
+			artist: `Fox Academy`,
+			url: 'https://www.youtube.com/watch?v=_aQuZaKdH9I'
+		},
+		{
+			title: `Off I Go`,
+			artist: `Greg Laswell`,
+			url: 'https://www.youtube.com/watch?v=4eg9DxVcYN8'
+		},
+		{
+			title: `O (Hidden Track)`,
+			artist: `Coldplay`,
+			url: 'https://www.youtube.com/watch?v=-gA3H3clEqk'
+		},
+		{
+			title: `Fine`,
+			artist: `Mal Blum`,
+			url: 'https://www.youtube.com/watch?v=Cdi9GkkGmec'
+		},
+		{
+			title: `Flying`,
+			artist: `James Newton Howard`,
+			url: 'https://www.youtube.com/watch?v=2yzDI_Jbm9w'
+		}
+	]
+}
