799afed80c5d5ee2f198abc4afe54baa425022ef
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2025-08-25 12:34:37 +0100

    better iframe detection system

--- neocities/js/iframe-detector.js
+++ neocities/js/iframe-detector.js
@@ -6,8 +6,27 @@ iFrameParams();
 
 */
 
+var isIFrame = false;
+
 // if this is being displayed in an iframe
 function iFrameParams() {
-	let urlParams = new URL(window.location.toLocaleString()).searchParams;
-	if ( urlParams.has('iframe') && urlParams.get('iframe') != 'false' ) document.body.classList.add('iframe');
+	// Validation: it must be loaded as the top page, or if it is loaded in an iframe 
+	// then it must be embedded in my own domain.
+	// Info: IF top.location.href is not accessible THEN it is embedded in an iframe 
+	// and the domains are different.
+	try {
+			var tophref = top.location.href;
+			var tophostname = top.location.hostname.toString();
+			var myhref = location.href;
+			if (tophref === myhref) {
+				isIFrame = false;
+			} else if (tophostname !== "www.yourdomain.com") {
+				isIFrame = true;
+			}
+	} catch (error) { 
+		// error is a permission error that top.location.href is not accessible 
+		// (which means parent domain <> iframe domain)!
+		isIFrame = true;
+	}
+	if (isIFrame) document.body.classList.add('iframe');
 }
