537324e2d2239e1095f8790a88e3c79d76369985
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2025-08-27 03:10:48 +0200

    newspaper section added

--- httpdocs/shrines/garf/script.js
+++ httpdocs/shrines/garf/script.js
@@ -1,2 +1,11 @@
 //force strict mode
 "use strict";
+
+// self executing
+window.addEventListener('load', ()=>{
+	let today = new Date();
+	const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+	const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+	let todayFormatted =`${weekday[today.getDay()]}, ${month[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+	document.getElementById('news-date').innerHTML = todayFormatted;
+});
