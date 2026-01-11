98bc8712c5b2be0b2a911ae891003a621f94327c
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-01-11 04:49:09 -0500

    redirect test environment on main site

--- codecreature.net/games/wormrace/test-environment/status.js
+++ codecreature.net/games/wormrace/test-environment/status.js
@@ -1,6 +1,7 @@
+if (window.location.includes("codecreature.net")) window.location.replace("test-environment/","");
+
 // force strict mode
 "use strict";
 
 // whether game is in maintenance mode
 var maintenance = false;
-if (window.location.includes("codecreature.net")) maintenance = true;
