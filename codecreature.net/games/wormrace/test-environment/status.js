0c96098d3324053b4de46733ce7423c1a39d06cf
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-01-11 04:53:04 -0500

    Delete codecreature.net/games/wormrace/test-environment directory

--- codecreature.net/games/wormrace/test-environment/status.js
+++
@@ -1,7 +0,0 @@
-if (window.location.includes("codecreature.net")) window.location.replace("test-environment/","");
-
-// force strict mode
-"use strict";
-
-// whether game is in maintenance mode
-var maintenance = false;
