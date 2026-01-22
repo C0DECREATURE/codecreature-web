0848bf40651bd9ff033d0bcd06cc82cbe94d786e
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-01-22 03:35:41 -0500

    vex code updates

--- codecreature.net/ocs/vex/vex.js
+++ codecreature.net/ocs/vex/vex.js
@@ -1,47 +1,64 @@
-// coffin img element
-var coffin;
-window.addEventListener('load',function(){ coffin = document.getElementById("coffin"); });
-
-// called when coffin is clicked
-function coffinClick() {
-	if (coffinLocked && document.querySelector('body').classList.contains('hasKey') ) unlockCoffin();
-	else toggleCoffin();
-}
-
-// open and close coffin image
-function toggleCoffin() {
-	if ( coffin.classList.contains('open') ) closeCoffin();
-	else openCoffin();
-}
-function openCoffin() {
-	if (!coffinLocked) {
-		coffin.classList.add('open'); coffin.classList.remove('closed'); 
-		coffin.querySelector('img').src = "images/coffin-open.png";
+const coffin = {
+	// coffin setup
+	init: ()=>{
+		coffin.btn = document.getElementById("coffin");
+		coffin.btn.addEventListener('click',()=>{ coffin.click(); });
+		coffin.key = document.getElementById("coffin-key");
+		coffin.key.addEventListener('click',()=>{ coffin.getKey(); });
+		// if user has already unlocked coffin in the past
+		if (localStorage.getItem('vexCoffinLocked') == "false") {
+			document.getElementById('coffin-key').remove();
+			coffin.unlock();
+		} else {
+			coffin.lock();
+		}
+	},
+	// called when coffin clicked
+	click: ()=>{
+		if (coffin.isLocked && document.querySelector('body').classList.contains('hasKey') ) coffin.unlock();
+		else coffin.toggle();
+	},
+	// toggle coffin open/closed state
+	toggle: ()=>{
+		if ( coffin.btn.classList.contains('open') ) coffin.close();
+		else coffin.open();
+	},
+	// open the coffin
+	open: ()=>{
+		if (!coffin.isLocked) {
+			coffin.btn.classList.toggle('open');
+			coffin.btn.querySelector('img').src = "images/coffin-open.png";
+		}
+	},
+	// close the coffin
+	close: ()=>{
+		coffin.btn.classList.toggle('open'); 
+		if (coffin.isLocked) coffin.btn.querySelector('img').src = "images/coffin-closed.png";
+		else coffin.btn.querySelector('img').src = "images/coffin-closed-unlocked.png";
+	},
+	// lock the coffin
+	lock: ()=>{
+		console.log("locking coffin");
+		coffin.isLocked = true;
+		localStorage.setItem('vexCoffinLocked',coffin.isLocked);
+	},
+	// unlock and open the coffin
+	unlock: ()=>{
+		console.log("unlocking coffin");
+		coffin.isLocked = false;
+		localStorage.setItem('vexCoffinLocked',coffin.isLocked);
+		coffin.useKey();
+		coffin.open();
+	},
+	// pick up the coffin key
+	getKey: ()=>{
+		document.querySelector('body').classList.add('hasKey');
+		document.getElementById('coffin-key').remove();
+		// if touch is primary input, unlock immediately
+		if (window.matchMedia("(pointer: coarse)").matches) { coffin.unlock(); }
+	},
+	// use the coffin key
+	useKey: ()=>{
+		document.querySelector('body').classList.remove('hasKey');
 	}
-}
-function closeCoffin() {
-	coffin.classList.add('closed'); coffin.classList.remove('open'); 
-	if (coffinLocked) coffin.querySelector('img').src = "images/coffin-closed.png";
-	else coffin.querySelector('img').src = "images/coffin-closed-unlocked.png";
-}
-
-// whether coffin is locked
-var coffinLocked = true;
-// unlock coffin
-function unlockCoffin() {
-	if ( document.querySelector('body').classList.contains('hasKey') ) {
-		coffinLocked = false;
-		useKey();
-		openCoffin();
-	}
-}
-
-// pick up coffin key
-function getKey() {
-	document.querySelector('body').classList.add('hasKey');
-	document.getElementById('coffin-key').remove();
-	// if touch is primary input, unlock immediately
-	if (window.matchMedia("(pointer: coarse)").matches) { unlockCoffin(); }
-}
-// use coffin key
-function useKey() { document.querySelector('body').classList.remove('hasKey'); }
+};
