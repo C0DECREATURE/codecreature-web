adf90007100745d0122c26f59890964132b8f564
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-01-24 23:49:52 -0500

    testing database access

--- codecreature.net/user/config.php
+++ codecreature.net/user/config.php
@@ -1,10 +1,9 @@
 <?php
-	/* Database credentials. Assuming you are running MySQL
-	server with default setting (user 'root' with no password) */
+	/* Database credentials */
 	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_NAME', 'users');
+	define('DB_USERNAME', 'user_login');
+	define('DB_PASSWORD', 'xeLXzdEmg9i75c!^');
+	define('DB_NAME', 'prax_users');
 	 
 	/* Attempt to connect to MySQL database */
 	$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
