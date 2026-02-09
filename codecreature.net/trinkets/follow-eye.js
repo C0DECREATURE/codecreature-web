509d793460a43881257c6a77c3d80c6846d964db
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-02-09 05:03:59 -0500

    trinkets: magnifier, spoons, rat, pearl eye

--- codecreature.net/trinkets/follow-eye.js
+++ codecreature.net/trinkets/follow-eye.js
@@ -1,7 +1,7 @@
 (()=>{
 	document.addEventListener('mousemove', (e)=>{
-		let x = e.clientX * 20 / window.innerWidth - 10;
-		let y = e.clientY * 20 / window.innerHeight - 10;
+		let x = e.clientX * 22 / window.innerWidth - 11;
+		let y = e.clientY * 22 / window.innerHeight - 11;
 		var pupils = document.getElementsByClassName('pupil');
 		for (let i = 0; i < pupils.length; i++) {
 			pupils[i].style.left = x + "%";
