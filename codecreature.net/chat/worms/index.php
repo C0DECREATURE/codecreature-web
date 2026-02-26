53b01a63357336cf8bc5a2f5576acd8fe54144d7
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-02-26 16:36:57 -0500

    chat styling

--- codecreature.net/chat/worms/index.php
+++ codecreature.net/chat/worms/index.php
@@ -59,7 +59,7 @@
 		<script src="/codefiles/page-settings.min.js?fileversion=20251216"></script>
 		
 		<!-- fonts -->
-		<script>fonts.load('Comic Sans MS','Super Comic')</script>
+		<script>fonts.load('Yet R','Super Comic')</script>
 		
 		<!-- svg icons -->
 		<script src="/graphix/svg-icons/svg-icons.js?fileversion=20251216" id="svg-icons-js"></script>
@@ -77,7 +77,20 @@
 </head>
 <body>
 	<main class="chatbox">
-		<header><a href="/user"><?php echo $logged_in ? $username : 'log in'; ?></a></header>
+		<header>
+			<a href="/user"><?php echo $logged_in ? $username : 'log in'; ?></a>
+			<button id="page-settings" class="open-page-settings" aria-label="page settings"
+			onclick="togglePageSettings();" onfocusvisible="togglePageSettings();">
+				<i class="svg-icon svg-icon-solid" data-icon="gear-solid" alt="">
+					<svg viewBox="0 0 132.29201 132.29201" xmlns="http://www.w3.org/2000/svg">
+						<path style="stroke-linejoin:round;stroke-linecap:butt;" d="M 58.475469,8.2100704 V 25.772997 A 41.184249,41.184253 0 0 0 35.106834,39.084493 L 20.308319,30.540394 12.64347,43.816052 27.439039,52.358188 a 41.184249,41.184253 0 0 0 -2.48311,13.787695 41.184249,41.184253 0 0 0 2.392781,13.545669 l -14.70524,8.490098 7.155267,13.56973 15.138236,-8.739987 a 41.184249,41.184253 0 0 0 23.538496,13.507377 v 16.9748 l 15.329698,0.58813 V 106.51877 A 41.184249,41.184253 0 0 0 97.497322,92.806186 l 15.493668,8.945194 7.15527,-13.56973 -15.124,-8.732125 A 41.184249,41.184253 0 0 0 107.3247,66.145883 41.184249,41.184253 0 0 0 105.01048,52.554559 L 120.14626,43.816052 112.4819,30.540394 97.340717,39.282337 A 41.184249,41.184253 0 0 0 73.805167,25.688558 V 8.2100704 Z m 7.664849,31.9490626 a 25.987005,25.986999 0 0 1 25.98724,25.98675 25.987005,25.986999 0 0 1 -25.98724,25.987241 25.987005,25.986999 0 0 1 -25.98675,-25.987241 25.987005,25.986999 0 0 1 25.98675,-25.98675 z" />
+					</svg>
+				</i>
+			</button>
+			<div id="page-settings-contents" class="page-settings-contents hidden" ariaLabel="page settings">
+				<!--page settings go here-->
+			</div>
+		</header>
 		
 		<section class="messages" id="messages">
 			<div class="hidden" id="latest-message-id">0</div>
@@ -107,7 +120,7 @@
 		</section>
 		
 		<form id="new-message" method="POST">
-			<input id="message-input" type="text" name="message" autocomplete="off"></input>
+			<input id="message-input" type="text" name="message" minlength="1" maxlength="400" autocomplete="off"></input>
 			<button type="submit" name="submit">send</button>
 		</form>
 	</main>
