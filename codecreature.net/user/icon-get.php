bf34fe96963ff825102fe8d6c8d3ad3f26bfa9e3
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-03-08 15:32:03 -0400

    function to get icon for given user

---
+++ codecreature.net/user/icon-get.php
@@ -0,0 +1,36 @@
+<?php
+
+// Include database connection file
+require_once "connect.php";
+
+function getIcon($id) {
+	global $users_conn;
+	
+	if (gettype($id) == "string") { $id = intval($id); }
+	
+	if(!empty($id)){
+		// prepared statement to get icon for user
+		$sql = "SELECT icon FROM users WHERE id = ?";
+		
+		if($stmt = mysqli_prepare($users_conn, $sql)){
+			// Bind variables to the prepared statement as parameters
+			mysqli_stmt_bind_param($stmt, "i", $param_id);
+			$param_id = $id;
+			
+			// Attempt to execute the prepared statement
+			if(mysqli_stmt_execute($stmt)){
+				// bind result variables
+				mysqli_stmt_bind_result($stmt, $icon);
+				// fetch values
+				while (mysqli_stmt_fetch($stmt)) {
+					return "/user/icons/".$icon.".png";
+				}
+			} else{
+				echo "Could not retrieve icon.";
+			}
+			
+			// Close statement
+			mysqli_stmt_close($stmt);
+		}
+	}
+}
