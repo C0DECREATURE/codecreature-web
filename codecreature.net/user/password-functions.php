a7d05a6d2bb14367a2dd69c5f165809ae2fdc5d7
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-03-15 23:34:48 -0400

    Period at end of password function error msg

--- codecreature.net/user/password-functions.php
+++ codecreature.net/user/password-functions.php
@@ -19,13 +19,13 @@ function getPasswordErr($p) {
 	} elseif (strlen(trim($p)) < $password_length) {
 		$password_err = "Password must have at least ".$password_length." characters.";
 	} elseif ( !preg_match('/(?=.*[0-9]).+/', trim($p)) ) {
-		$password_err = "Password must contain at least one number";
+		$password_err = "Password must contain at least one number.";
 	} elseif ( !preg_match('/(?=.*[a-zA-Z]).+/', trim($p)) ) {
-		$password_err = "Password must contain at least one letter";
+		$password_err = "Password must contain at least one letter.";
 	} elseif(preg_match('/(.)\1{3}/', trim($p))){
-		$password_err = "Password may not have more than 3 repeating characters";
+		$password_err = "Password may not have more than 3 repeating characters.";
 	} elseif ( preg_match('/(?=.*(?=(\w+)\1{2}|p[a@*][s\$][s\$]|w[o0*]rd|user|login|access|qwerty|q2w3e|uiop|asdf|monkey|letmein|dragon|hello|batman|shadow|loveme|lovely|mynoob|donald|charlie|ashley|hottie|flower|abc|xyz|012|123|345|456|567|678|789|890|1122|2233|4455|5566|6677|8899|9900)).+/i', trim($p)) ) {
-		$password_err = "Please choose a less common password";
+		$password_err = "Please choose a less common password.";
 	}
 	return $password_err;
 }
