14ac651fea8a67c211e2946eb4a2c1d55035cf21
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-03-16 12:42:18 -0400

    changing user database.php func to procedural

--- codecreature.net/user/database.php
+++ codecreature.net/user/database.php
@@ -5,18 +5,36 @@ require_once "connect.php";
 
 function getUsername($id) {
 	global $users_conn;
-	$sql = "SELECT username FROM users WHERE id = ".$id;
-	// Execute the SQL query
-	$result = $users_conn->query($sql);
-
-	// Process the result set
-	if ($result->num_rows > 0) {
-		// Output data of each row
-		while($row = $result->fetch_assoc()) {
-			return $row["username"];
-		}
+	
+	if (gettype($id) == "string") { $id = intval($id); }
+	
+	if ($id == 0 || empty($id)) {
+		return "Anonymous";
 	} else {
-		echo "User #".$id;
+		// prepared statement to get icon for user
+		$sql = "SELECT username FROM users WHERE id = ?";
+		
+		if($stmt = mysqli_prepare($users_conn, $sql)){
+			// Bind variables to the prepared statement as parameters
+			mysqli_stmt_bind_param($stmt, "i", $param_id);
+			$param_id = $id;
+			
+			// Attempt to execute the prepared statement
+			if(mysqli_stmt_execute($stmt)){
+				// bind result variables
+				mysqli_stmt_bind_result($stmt, $username);
+				// fetch values
+				while (mysqli_stmt_fetch($stmt)) {
+					if (empty($username)) { $username = "Missing Username"; }
+					return $username;
+				}
+			} else{
+				echo "Could not retrieve username.";
+			}
+			
+			// Close statement
+			mysqli_stmt_close($stmt);
+		}
 	}
 }
 
