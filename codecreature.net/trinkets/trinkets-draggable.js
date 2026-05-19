b4bc6ffe5f53682e4a60165ec0dda02806372f59
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-05-19 09:54:58 -0400

    not working yet

--- codecreature.net/trinkets/trinkets-draggable.js
+++ codecreature.net/trinkets/trinkets-draggable.js
@@ -1,12 +1,48 @@
-// Make the DIV element draggable:
+// Make the element draggable:
 dragElement(document.getElementById("drag-wrapper"));
-//document.getElementById("drag-wrapper").addEventListener('touchstart',mouseDown,false);
 
 /* whether the page can currently be dragged */
 let pageDraggable = true;
-console.log(pageDraggable);
 
+//let pos = { top: 0, left: 0, x: 0, y: 0 };
+/* 
+function dragScrollElement(ele) {
+	
+	ele.addEventListener('mousedown', mouseDownHandler);
+	ele.addEventListener('touchstart', mouseDownHandler);
+
+	function mouseDownHandler(e) {
+		pos = {
+			// current scroll
+			left: ele.scrollLeft,
+			top: ele.scrollTop,
+			// current mouse pos
+			x: e.clientX,
+			y: e.clientY,
+		};
+		document.addEventListener('mousemove', mouseMoveHandler);
+		document.addEventListener('mouseup', mouseUpHandler);
+	};
+	function mouseMoveHandler(e) {
+		// How far the mouse has been moved
+		const dx = e.clientX - pos.x;
+		const dy = e.clientY - pos.y;
+
+		// Scroll the element
+		ele.scrollTop = pos.top - dy;
+		ele.scrollLeft = pos.left - dx;
+	};
+	// reset on mouseup
+	function mouseUpHandler() {
+		document.removeEventListener('mousemove', mouseMoveHandler);
+		document.removeEventListener('mouseup', mouseUpHandler);
+	};
+} */
+
+ 
 function dragElement(elmnt) {
+	elmnt.scrollLeft = 0;
+	elmnt.scrollTop = 0;
 	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 	// move the DIV from anywhere inside the DIV:
 	elmnt.addEventListener('mousedown', dragMouseDown);
@@ -43,11 +79,11 @@ function dragElement(elmnt) {
 			// get bottom right corner marker
 			let bottomRightRect = document.getElementById("bottom-right-marker").getBoundingClientRect();
 			// prevent dragging too far up
-			let curTop = Number(elmnt.style.top.replaceAll("px",""));
+			let curTop = Number(elmnt.scrollTop);
 			let maxTopMove = bottomRightRect.bottom - window.innerHeight;
 			top = Math.max(top, curTop-maxTopMove);
 			// prevent dragging too far left
-			let curLeft = Number(elmnt.style.left.replaceAll("px",""));
+			let curLeft = Number(elmnt.scrollLeft);
 			let maxLeftMove = bottomRightRect.right - window.innerWidth;
 			left = Math.max(left, curLeft-maxLeftMove);
 			// prevent dragging too far down
@@ -57,8 +93,8 @@ function dragElement(elmnt) {
 			
 			//if (bottomRightRect.left > 0) { left = Math.min(Number(elmnt.style.left.replaceAll("px","")), left); }
 			// set the element's new position:
-			elmnt.style.top = top + "px";
-			elmnt.style.left = left + "px";
+			elmnt.scrollTop = top;
+			elmnt.scrollLeft = left;
 		}
 	}
 
