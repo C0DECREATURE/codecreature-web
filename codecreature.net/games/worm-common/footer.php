5150ce10bc33a556263bd5211a21f0d0fd11113a
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-05-30 13:43:25 -0700

    worm chat iframe hidden until COMPLETELY loaded

--- codecreature.net/games/worm-common/footer.php
+++ codecreature.net/games/worm-common/footer.php
@@ -12,19 +12,33 @@
 			Worm Chat
 			<button id="chat-button">—</button>
 		</header>
-		<iframe id="chat-iframe"></iframe>
+		<iframe id="chat-iframe" class="hidden"></iframe>
 		<div class="iframe-load-preview">Loading...</div>
 	</div>
 	<script>
 		(()=>{
 			let chatboxLoaded = false;
+			let iFrame = document.getElementById('chat-iframe');
+			let chatWrapper = document.getElementById('chatbox');
+					
+			function checkIframeLoaded() {
+				console.log('check');
+				let iFrameDoc = iFrame.contentDocument || iFrame.contentWindow.document;
+				if (iFrameDoc && iFrameDoc.readyState == 'complete') showIframe();
+				else window.setTimeout(checkIframeLoaded, 100);
+			}
+			function showIframe() {
+				iFrame.classList.remove('hidden');
+				chatWrapper.querySelector('.iframe-load-preview').classList.add('hidden');
+			}
 			function chatButtonClicked() {
+				chatWrapper.classList.toggle('hidden');
 				if (!chatboxLoaded) {
 					chatboxLoaded = true;
-					document.getElementById('chat-iframe').src = "/chat/worms";
-					document.getElementById('chatbox').querySelector('.iframe-load-preview').classList.add('hidden');
+					iFrame.src = "/chat/worms";
+					window.setTimeout(checkIframeLoaded, 300);
 				}
-				document.getElementById('chatbox').classList.toggle('hidden');
+				else showIframe();
 			}
 			document.getElementById('chat-button').addEventListener('click',chatButtonClicked);
 			document.getElementById('open-chatbox').addEventListener('click',chatButtonClicked);
