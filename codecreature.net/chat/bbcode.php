04f4c476b19af672576106f436c12267a4768d47
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-06-26 03:23:28 -0400

    long worm emojis - blue, pink, purple

--- codecreature.net/chat/bbcode.php
+++ codecreature.net/chat/bbcode.php
@@ -14,12 +14,25 @@ $bbcode->AddSmiley(":left:","arrow_left.png");
 $bbcode->AddSmiley(":up:","arrow_up.png");
 $bbcode->AddSmiley(":down:","arrow_down.png");
 $bbcode->AddSmiley(":star:","star.png");
+
 $bbcode->AddSmiley(":jeremy:","worm_pink.png"); $bbcode->AddSmiley(":Jeremy:","worm_pink.png");
 $bbcode->AddSmiley(":pretzel:","worm_orange.png"); $bbcode->AddSmiley(":Pretzel:","worm_orange.png");
 $bbcode->AddSmiley(":stringcheese:","worm_yellow.png"); $bbcode->AddSmiley(":StringCheese:","worm_yellow.png"); $bbcode->AddSmiley(":string_cheese:","worm_yellow.png"); $bbcode->AddSmiley(":string-cheese:","worm_yellow.png");
 $bbcode->AddSmiley(":matilda:","worm_green.png"); $bbcode->AddSmiley(":Matilda:","worm_green.png");
 $bbcode->AddSmiley(":poolnoodle:","worm_blue.png"); $bbcode->AddSmiley(":PoolNoodle:","worm_blue.png"); $bbcode->AddSmiley(":pool_noodle:","worm_blue.png"); $bbcode->AddSmiley(":pool-noodle:","worm_blue.png");
 $bbcode->AddSmiley(":microplastics:","worm_purple.png"); $bbcode->AddSmiley(":Microplastics:","worm_purple.png");
+// long blue worm
+$bbcode->AddSmiley(":poolnoodlehead:","long_worm_blue_3.png"); $bbcode->AddSmiley(":poolnoodle_head:","long_worm_blue_3.png"); $bbcode->AddSmiley(":pool_noodle_head:","long_worm_blue_3.png");
+$bbcode->AddSmiley(":poolnoodlebody:","long_worm_blue_2.png"); $bbcode->AddSmiley(":poolnoodle_body:","long_worm_blue_2.png"); $bbcode->AddSmiley(":pool_noodle_body:","long_worm_blue_2.png");
+$bbcode->AddSmiley(":poolnoodletail:","long_worm_blue_1.png"); $bbcode->AddSmiley(":poolnoodle_tail:","long_worm_blue_1.png"); $bbcode->AddSmiley(":pool_noodle_tail:","long_worm_blue_1.png");
+// long purple worm
+$bbcode->AddSmiley(":microplasticshead:","long_worm_purple_3.png"); $bbcode->AddSmiley(":microplastics_head:","long_worm_purple_3.png");
+$bbcode->AddSmiley(":microplasticsbody:","long_worm_purple_2.png"); $bbcode->AddSmiley(":microplastics_body:","long_worm_purple_2.png");
+$bbcode->AddSmiley(":microplasticstail:","long_worm_purple_1.png"); $bbcode->AddSmiley(":microplastics_tail:","long_worm_purple_1.png");
+// long pink worm
+$bbcode->AddSmiley(":jeremyhead:","long_worm_pink_3.png"); $bbcode->AddSmiley(":jeremy_head:","long_worm_pink_3.png");
+$bbcode->AddSmiley(":jeremybody:","long_worm_pink_2.png"); $bbcode->AddSmiley(":jeremy_body:","long_worm_pink_2.png");
+$bbcode->AddSmiley(":jeremytail:","long_worm_pink_1.png"); $bbcode->AddSmiley(":jeremy_tail:","long_worm_pink_1.png");
 
 // automatically detect and style links
 $bbcode->SetDetectURLs(true);
